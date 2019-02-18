beea7315184fac0daa7ff45d4cc7394a0ffa1337
Ivan Christian <ivan_christian@mymail.sutd.edu.sg>
2019-02-19 01:54:07 +0800

    Updated Finished Tutorial 3
    Need to prepare card art etc.

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//Making Card Object for the Game
+
+namespace SA
+
+{
+    [CreateAssetMenu(menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string cardName; // Card Name Place Holder
+        public Sprite art; //Art Sprite in the Scene
+        public string cardDetails; //Card Effects
+        public string cardFlavour; //Card Details
+        public string source; // Crediting the source of the card art
+         
+    }
+}
---
+++ Assets/Scripts/Cards/CardViz.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+//Loading card object and everything in it.
+
+
+
+
+namespace SA
+{
+    public class CardViz : ScriptableObject
+    {
+        public Text title;
+        public Text details;
+        public Text flavor;
+        public Image art;
+        public Text source;
+
+        //Creating Card Object
+        public Card card;
+
+        void Start()
+        {
+            LoadCard(card);
+            //Need to have card prefabs in the prefabs folder
+        }
+
+
+
+        public void LoadCard(Card c)
+        {
+            if (c == null)
+            {
+                return;
+            }
+
+
+            card = c;
+            title.text = c.cardName;
+            details.text = c.cardDetails;
+            flavor.text = c.cardFlavour;
+            source.text = c.source;
+            art.sprite = c.art;
+        }
+
+    }
+}
