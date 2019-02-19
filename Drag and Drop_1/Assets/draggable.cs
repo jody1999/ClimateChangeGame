f431980e698fa365895314d403ceb4dbf1654b96
Sumgikarp <limsamantha000@gmail.com>
2019-02-19 18:20:53 +0800

    drag n drop

---
+++ Drag and Drop_1/Assets/draggable.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+    public Transform placeholderParent = null;
+    public enum Slot { WEAPON, HEAD, CHEST, LEGS, FEET, INVENTORY };
+    public Slot typeOfItem = Slot.WEAPON;
+
+    GameObject placeholder = null;
+    
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        parentToReturnTo = this.transform.parent;
+        placeholderParent = parentToReturnTo;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        //finad all dropzones in the game, for a valid slot for my card(below)
+        //dropzone[] zones = GameObject.FindObjectOfType<dropzone>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+        this.transform.position = eventData.position;
+
+        if (placeholder.transform.parent != placeholderParent)
+        {
+            placeholder.transform.SetParent(placeholderParent);
+        }
+
+        //int newSiblingIndex = placeholder.transform.GetSiblingIndex();
+        //int newSiblingIndex = parentToReturnTo.childCount;
+        int newSiblingIndex = placeholderParent.childCount;
+
+
+        //rearranging cards
+        //change parentToReturnTo to placeholderParent
+        for (int i = 0; i < placeholderParent.childCount; i++)
+        {
+            if(this.transform.position.x < placeholderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+                
+                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                {
+                    newSiblingIndex--;
+                }                  
+                
+                //placeholder.transform.SetSiblingIndex(i);
+                break;
+            }
+        }
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(parentToReturnTo);
+        //goes back to original position(place) when i move a card away from its original position and release elsewhere
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        //unless Raycast is not enabled, log will tell every single object that's underneath the mouse
+        //EventSystem.current.RaycastAll(eventData);
+
+        Destroy(placeholder);
+    }
+}
